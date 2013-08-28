cbe9634ac526f98d4487d5abb085723ec41e2e29
Mircea Moise <mmoise@thoughtworks.com>
2013-08-28 20:33:56 +0100

    Mircea : Added signup views and db

--- routes.rb
+++ routes.rb
@@ -1,4 +1,11 @@
 require 'sinatra'
+require "dm-core"
+#for using auto_migrate!
+require "dm-migrations"
+require "digest/sha1"
+require "sinatra-authentication"
+
+use Rack::Session::Cookie, :secret => 'a very secret key'
 
 if ARGV.size > 0
   config_file = ARGV[0]
@@ -11,13 +18,18 @@ require config_file
 require_relative 'src/book_controller'
 require_relative 'src/mapper'
 
+DataMapper.auto_migrate!
+
 before do
   @book_controller = BookController.new
 end
 
+
 get '/' do
-	'Service is up'
+  send(:erb,"<h1>fasf</h1>",:layout => false)
+
 end
+
 
 post '/books' do
   book_hash = {:title => @request["title"], :owner => @request["owner"]}
