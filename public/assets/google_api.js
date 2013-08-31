0d3d9e4d662be45d61b09beccfbbfade5af9d269
Mircea Moise <mmoise@thoughtworks.com>
2013-08-31 19:03:02 +0100

    Mircea : Added popup for searching books

--- public/assets/google_api.js
+++ public/assets/google_api.js
@@ -1,6 +1,14 @@
 
 function handle_response(response) {
    console.log(response);
+    $("#results_list").html("");
+   for (var i = 0; i < response.items.length; i++) {
+       var item = response.items[i];
+       console.log(item);
+//       alert("<tr><td> <img src='"+item.volumeInfo.imageLinks.smallThumbnail+"'/></td><td>"+item.volumeInfo.title+"</td></tr>");
+       var tr = $("#results_list").append("<tr><td> <img src='"+item.volumeInfo.imageLinks.smallThumbnail+"'/></td><td>"+item.volumeInfo.title+"</td></tr>");
+
+   }
 }
 
 function search_for_book (query) {
@@ -10,3 +18,9 @@ function search_for_book (query) {
         dataType: "jsonp"
     });
 }
+
+$(document).ready(function() {
+    $('#search_modal').on('shown', function() {
+        search_for_book($("#title").val());
+    })
+});
