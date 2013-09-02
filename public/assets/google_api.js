7ddc6a9122cea0711d0df0700c6bf58065cda393
Mircea Moise <mmoise@thoughtworks.com>
2013-09-02 21:38:50 +0100

    Mircea : Added isbn field and refactored the way selecting the book worked in js

--- public/assets/google_api.js
+++ public/assets/google_api.js
@@ -6,8 +6,10 @@ function handle_response(response) {
        var item = response.items[i];
        console.log(item);
 //       alert("<tr><td> <img src='"+item.volumeInfo.imageLinks.smallThumbnail+"'/></td><td>"+item.volumeInfo.title+"</td></tr>");
-       var tr = $("#results_list").append("<tr><td> <img src='"+item.volumeInfo.imageLinks.smallThumbnail+"'/></td><td>"+item.volumeInfo.title+"</td></tr>");
+       var tr = $("<tr><td> <img src='"+item.volumeInfo.imageLinks.smallThumbnail+"'/></td><td>"+item.volumeInfo.title+"</td></tr>");
 
+       $("#results_list").append(tr);
+       tr.data("item",item);
    }
     $("#results_list tr").click(function (e) {$("#results_list tr").removeClass("success");$(e.currentTarget).addClass("success")});
 }
@@ -25,7 +27,15 @@ $(document).ready(function() {
         search_for_book($("#title").val());
     })
     $('#search_modal').on('hide', function() {
-        var selected_title = $("#results_list tr.success").children()[1].innerHTML;
+        var selected_element = $("#results_list tr.success");
+        var data = selected_element.data("item");
+        var selected_title = data.volumeInfo.title;
+        var isbn_13 = "";
+        for (var i in data.volumeInfo.industryIdentifiers){
+            if (data.volumeInfo.industryIdentifiers[i].type == "ISBN_13") isbn_13 = data.volumeInfo.industryIdentifiers[i].identifier;
+        }
+
         $("#title").val(selected_title);
+        $("#isbn").val(isbn_13)
     })
 });
