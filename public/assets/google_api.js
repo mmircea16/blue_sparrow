7f06e46ed88fa7ea2e3f6a55927577c5d113fc01
Mircea Moise <mmoise@thoughtworks.com>
2013-09-05 20:36:53 +0100

    Mircea : Changed style for modal window for searching books

--- public/assets/google_api.js
+++ public/assets/google_api.js
@@ -12,6 +12,7 @@ function handle_response(response) {
        tr.data("item",item);
    }
     $("#results_list tr").click(function (e) {$("#results_list tr").removeClass("success");$(e.currentTarget).addClass("success")});
+    $("#results_list tr").css("cursor","pointer");
 }
 
 function search_for_book (query) {
@@ -35,17 +36,21 @@ function get_isbn_13(data) {
     }
     return "";
 }
+
+function select_book(){
+    var selected_element = $("#results_list tr.success");
+    var data = selected_element.data("item");
+    var selected_title = data.volumeInfo.title;
+    var isbn_13 = get_isbn_13(data);
+
+    $("#title").val(selected_title);
+    $("#isbn").val(isbn_13);
+    console.log("select book");
+}
+
 $(document).ready(function() {
     $('#search_modal').on('shown', function() {
         search_for_book($("#title").val());
     })
-    $('#search_modal').on('hide', function() {
-        var selected_element = $("#results_list tr.success");
-        var data = selected_element.data("item");
-        var selected_title = data.volumeInfo.title;
-        var isbn_13 = get_isbn_13(data);;
 
-        $("#title").val(selected_title);
-        $("#isbn").val(isbn_13)
-    })
 });
